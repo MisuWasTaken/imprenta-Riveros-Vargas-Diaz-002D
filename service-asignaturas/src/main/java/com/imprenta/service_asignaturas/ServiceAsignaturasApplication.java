9d106ba537ab5562a7325aef9b963bb0e32fe421
Misuzu <panchitoriveros@gmail.com>
2026-05-10 16:01:50 -0400

    cree service asignaturas

---
+++ service-asignaturas/src/main/java/com/imprenta/service_asignaturas/ServiceAsignaturasApplication.java
@@ -0,0 +1,13 @@
+package com.imprenta.service_asignaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAsignaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAsignaturasApplication.class, args);
+	}
+
+}
---
+++ service-asignaturas/src/test/java/com/imprenta/service_asignaturas/ServiceAsignaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imprenta.service_asignaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAsignaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
