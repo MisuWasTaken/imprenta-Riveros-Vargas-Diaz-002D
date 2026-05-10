2acbacc05fda5ea8f3e2141b327be79143a3a45e
Misuzu <panchitoriveros@gmail.com>
2026-05-10 15:57:41 -0400

    Cree service profesores

---
+++ service-profesores/src/main/java/com/imprenta/service_profesores/ServiceProfesoresApplication.java
@@ -0,0 +1,13 @@
+package com.imprenta.service_profesores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProfesoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProfesoresApplication.class, args);
+	}
+
+}
---
+++ service-profesores/src/test/java/com/imprenta/service_profesores/ServiceProfesoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imprenta.service_profesores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProfesoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
