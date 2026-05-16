23de5ba73cdb831247d5b54455ebd42cb2027c78
emodarck <geo.vargas@duocuc.cl>
2026-05-16 15:35:23 -0400

    elimine todo lo antiguo de impresiones

--- imprenta/src/main/java/com/imprenta/imprenta/repository/a/ImpresionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.imprenta.imprenta.repository.a;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.imprenta.imprenta.model.ImpresionModel;
-
-@Repository
-public interface ImpresionRepository extends JpaRepository<ImpresionModel, Integer> {
-}
