3c4eb15a8794577d4e1a27a6a2291ad07fcf9525
Misuzu <panchitoriveros@gmail.com>
2026-05-16 02:52:57 -0400

    cree impresiones

---
+++ service-impresiones/src/main/java/com/imprenta/service_impresiones/ServiceImpresionesApplication.java
@@ -0,0 +1,13 @@
+package com.imprenta.service_impresiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceImpresionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceImpresionesApplication.class, args);
+	}
+
+}
---
+++ service-impresiones/src/test/java/com/imprenta/service_impresiones/ServiceImpresionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imprenta.service_impresiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceImpresionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
