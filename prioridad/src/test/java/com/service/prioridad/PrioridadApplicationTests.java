456fa3c083d8beda4aede9340351b866da027dbb
CharoTontito <dam.diazp@duocuc.cl>
2026-06-11 19:14:12 -0400

    calidad y prioridad creados

---
+++ prioridad/src/test/java/com/service/prioridad/PrioridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.prioridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrioridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
